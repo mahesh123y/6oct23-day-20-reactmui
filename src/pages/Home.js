bbf204c82fcdf3444b53a0568f7884bf20aff607
Mahesh Yadav <ymahesh756@gmail.com>
2023-10-10 07:44:30 +0530

    c1

---
+++ src/pages/Home.js
@@ -0,0 +1,42 @@
+
+import { Box, Typography, styled } from '@mui/material'
+import { red } from '@mui/material/colors';
+import React from 'react'
+
+
+const Wrapper = styled(Box)(({ theme }) => ({
+  background: "red",
+  width: "100%",
+  height: "100vh",
+  [theme.breakpoints.down("md")]: {
+    background: "orange",
+  },
+  [theme.breakpoints.down("sm")]: {
+    background: "blue",
+  },
+  [theme.breakpoints.up("lg")]: {
+    background: "purple",
+  },
+}));
+
+const Heading = styled(Typography)(({ theme }) => ({
+  textAlign: "center",
+  color: "white",
+  paddingTop: "3rem",
+  fontWeight: 600,
+  [theme.breakpoints.down("md")]: {
+    fontSize: "1.5rem",
+  },
+}))
+
+const Home = () => {
+  return (
+    <>
+      <Wrapper>
+        <Heading variant="h2">This is a Heading tag</Heading>
+      </Wrapper>
+    </>
+  )
+}
+
+export default Home
---
+++ test.js
@@ -0,0 +1,11 @@
+//Function define
+
+function mahesh(a){
+    return a + 10;
+
+}
+
+// 2. Function calling/invocation
+ let x = mahesh(5);
+
+ console.log(x+1);
